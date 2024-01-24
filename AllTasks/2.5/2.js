642e1ae75325c04f2d57b40876ce950d9a6f9c0a
Bergi-bear <tarantyl@inbox.ru>
2024-01-24 16:39:28 +0300

    продолжила выполнять задания

--- AllTasks/2.5/2.js
+++ AllTasks/2.5/2.js
@@ -2,7 +2,7 @@
 // Удалите из этой строки каждый третий символ. В нашем случае должно получится следующее: 'abdeg'
 let str = 'abcdefg';
 let arr =[...str];
-for (let i=3;i<str.length;i+=2){
-    arr=str.splice(i,1)
+for (let i=2;i<str.length;i+=2){
+    arr.splice(i,1)
 }
-console.log(arr)
+console.log(arr.join(""))
--- AllTasks/2.5/3.js
+++ AllTasks/2.5/3.js
@@ -1,2 +1,19 @@
 //Дан некоторый массив, например, вот такой: [1, 2, 3, 4, 5, 6]
 // Поделите сумму элементов, стоящих на четных позициях, на сумму элементов, стоящих на нечетных позициях.
+let arr = [1, 2, 3, 4, 5, 6]
+let arrEven=[]
+let arrOdd = []
+let sumArrEven=0,sumArrOdd=0
+for (let i=0;i<arr.length;i++){
+    if (i%2==0){
+        arrEven.push(arr[i])
+    }
+    else arrOdd.push(arr[i])
+}
+for (let i=0;i<arrEven.length;i++){
+    sumArrEven+=arrEven[i]
+}
+for (let i=0;i<arrOdd.length;i++){
+    sumArrOdd+=arrOdd[i]
+}
+console.log(sumArrEven/sumArrOdd)
